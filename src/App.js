c92c525a878d7f8863819823bc06d492384c01d3
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2022-06-28 22:39:57 +0300

    create components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Costs from "./components/Costs/Costs";
 
-function App() {
+const App = () => {
+  const costs = [
+    {
+      date: new Date(2021, 2, 12),
+      description:'Fridge',
+      amount:999.99
+    },
+    {
+      date: new Date(2021, 11, 25),
+      description:'MacBook',
+      amount:999.99
+    },
+    {
+      date: new Date(2021, 1, 3),
+      description:'Jeans',
+      amount:999.99
+    }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Begin learn react</h1>
+      <Costs costs = {costs}/>
+      
     </div>
   );
 }
---
+++ src/components/Costs/CostItem.js
@@ -0,0 +1,22 @@
+import './CostItem.css'
+import CostDate from './CostDate';
+import Card from '../UI/Card';
+
+
+const CostItem = (props) => {
+ 
+  return (
+    <div>
+      <Card className='cost-item'>
+
+        <CostDate date={props.date}/>
+      <div className='const-item__description'>
+        <h2>{props.description}</h2>
+        <div className='cost-item__price'>{props.amount}</div>
+      </div>
+      </Card>
+    </div>
+  );
+}
+
+export default CostItem;
